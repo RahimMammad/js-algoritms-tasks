dff5834ed640f622d393d209d0a2ad00597be795
Rahim <103603909+RahimMammad44@users.noreply.github.com>
2023-10-15 23:39:30 +0400

    tasks

---
+++ main.js
@@ -0,0 +1,39 @@
+//////////////////////task1//////////////////////
+
+// const array = [2, 1, 3, 4, 6]
+// let minimum = array[0];
+// let maximum = array[0];
+// let sum = 0;
+
+// for (let i = 0; i < array.length; i++) {
+//     if (array[i] < minimum) {
+//       minimum = array[i];
+//     }
+//     if (array[i] > maximum) {
+//       maximum = array[i];
+//     }
+//     sum += array[i];
+//   }
+//   sum = sum - minimum - maximum;
+
+// console.log(sum)
+
+////////////////////task4/////////////////////
+
+// const array = [123,7,53,99]
+// let minimum = array[0];
+// let maximum = array[0];
+// let sum = 0;
+
+// for (let i = 0; i < array.length; i++) {
+//     if (array[i] < minimum) {
+//       minimum = array[i];
+//     }
+//     if (array[i] > maximum) {
+//       maximum = array[i];
+//     }
+//     sum += array[i];
+//   }
+//   sum = (maximum + minimum) / 2
+
+// console.log(sum)
